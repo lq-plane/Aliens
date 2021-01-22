26aed59618ffbed0a5aefa7a108e7a1acd379f59
lq_glb <13516383930@163.com>
2021-01-23 00:51:05 +0800

    first commit

---
+++ ship.py
@@ -0,0 +1,40 @@
+import pygame
+from pygame.sprite import Sprite
+class Ship(Sprite):
+    def __init__(self,ai_settings,screen):
+        #初始化飞船，并设置其属性
+        super(Ship, self).__init__()
+        #是飞船对象拥有属性
+        self.screen=screen
+        #将游戏设置参数作为ship对象属性
+        self.ai_settings=ai_settings
+        #加载飞船图像
+        self.image=pygame.image.load('images/ship.bmp')
+        #飞船的外接矩形
+        self.rect=self.image.get_rect()
+        #屏幕screen的外接矩形
+        self.screen_rect=screen.get_rect()
+
+        #飞船初始位置，纵向底部对齐
+        #飞船矩形中心点
+        self.rect.centerx=self.screen_rect.centerx
+        #飞船矩形纵部坐标
+        self.rect.bottom=self.screen_rect.bottom
+        #在飞船属性center中存储小数值
+        self.center=float(self.rect.centerx)
+
+        #移动标致
+        self.moving_right=False
+        self.moving_left = False
+
+    def update(self):
+        if self.moving_right and self.rect.right<self.screen_rect.right:
+            self.center+=self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left>0:
+            self.center-=self.ai_settings.ship_speed_factor
+        self.rect.centerx=self.center#
+    def blitme(self):
+        self.screen.blit(self.image,self.rect)
+    def center_ship(self):
+        #让飞船在屏幕上居中
+        self.center=self.screen_rect.centerx
